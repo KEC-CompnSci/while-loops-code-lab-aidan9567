1139ddbf3de961627141695c940d5caefa22ea36
aidan9567 <27friesa@RETSDstudent.ca>
2025-03-19 19:20:24 +0000

    sss

--- src/assignment.py
+++ src/assignment.py
@@ -8,6 +8,11 @@ Replace this with your actual assignment content.
 # Task 1: Count by twos and print all even numbers from 2 to 50 (inclusive)
 # Write your code for Task 1 below:
 # TODO: Write a while loop that counts by 2s from 2 to 50 and prints each number
+count = 0
+while count <= 50:
+    print(count)
+    count += 2
+
 
 print("Task 1 complete!\n")
 
@@ -18,5 +23,13 @@ print("Task 1 complete!\n")
 # There should be 5 rows in total
 #remember to print without a new line use print("message", end="")
 
+row = 0
+while row < 5:
+    yeah = 0
+    while yeah < 5:
+        print("#", end="")  
+        yeah += 1
+    print()
+    row += 1
 
 print("Task 2 complete!")
